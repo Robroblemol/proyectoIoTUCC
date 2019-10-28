3c94686c4900ddadeadcc3127e34b40e23c71b9d
Raziel <robroblemol>
2019-10-28 13:06:55 -0500

    testing led input to Firebase

--- front/src/components/Devices.tsx
+++ front/src/components/Devices.tsx
@@ -1,19 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import firebase from 'firebase';
 import { useObject, useObjectVal } from 'react-firebase-hooks/database'
+import { firebaseConfig } from '../firebaseConfig'
 //import EditForm from './EditForm';
 
 import './Devices.css';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCc0omLy_FecUPTNrghJRPwPsysKdbsl6U",
-  authDomain: "iot-service-d3338.firebaseapp.com",
-  databaseURL: "https://iot-service-d3338.firebaseio.com",
-  projectId: "iot-service-d3338",
-  storageBucket: "iot-service-d3338.appspot.com",
-  messagingSenderId: "408382735005",
-  appId: "1:408382735005:web:7ad90c0c2fb5b403fa0a74"
-};
 
 firebase.initializeApp(firebaseConfig);
 
@@ -27,11 +19,17 @@ const Device =() => {
 
   const [snapshots={...getInitialState()}, loading, error] = useObjectVal(firebase.database().ref());
   // const [state, setState] = useState({...getInitialState()})
+  // console.log('snapshots', snapshots);
+  // console.log('error',error);
+  // console.log('loading', loading);
+
+  const handleLed = (event:any) => {
+    // console.log('envent click: ',event);
+    firebase.database().ref('led').set( !snapshots.led );
+    
+  }
   
-  console.log('snapshots', snapshots);
-  console.log('error',error);
-  console.log('loading', loading);
-  
+  // const snapshots = {...getInitialState()}
 
   
 
@@ -42,6 +40,12 @@ const Device =() => {
               <p> led: {snapshots.led ? 'encendido': 'apagado'}</p>
               <p>ADC: {snapshots.pot} </p>
               <p> time: {snapshots.time}</p>
+              <button 
+              onClick = {handleLed}
+              name= 'led'
+              id= 'led'>
+                {!snapshots.led ? 'Encender led': 'Apagar led'}
+              </button>
           </div>
         );
   }
